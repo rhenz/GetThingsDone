09061dfe3f07662419b83bb9174d2e31c2b1c273
rhenz <salvador.rhenz@gmail.com>
2022-09-07 19:38:17 +0800

    Create AppCoordinator(Coordinator Pattern) that will set the initial root view controller of the app

---
+++ GetThingsDone/Coordinators/AppCoordinator.swift
@@ -0,0 +1,37 @@
+//
+//  AppCoordinator.swift
+//  GetThingsDone
+//
+//  Created by John Salvador on 9/7/22.
+//
+
+import UIKit
+
+protocol Coordinator {
+    var childCoordinators: [Coordinator] { get }
+    
+    func start()
+}
+
+final class AppCoordinator: Coordinator {
+    
+    // MARK: - Properties
+    
+    private(set) var childCoordinators: [Coordinator] = []
+    private let window: UIWindow
+    
+    // MARK: - Init
+    
+    init(window: UIWindow) {
+        self.window = window
+    }
+    
+    // MARK: -
+    
+    func start() {
+        let navVC = UINavigationController()
+        
+        window.rootViewController = navVC
+        window.makeKeyAndVisible()
+    }
+}
