64673e451e625e3471b331776166c8af0c7a8c03
rhenz <salvador.rhenz@gmail.com>
2022-09-07 20:09:03 +0800

    Create `WelcomeViewController`

---
+++ GetThingsDone/Modules/Welcome/WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WelcomeViewController.swift
+//  GetThingsDone
+//
+//  Created by John Salvador on 9/7/22.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    // MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
