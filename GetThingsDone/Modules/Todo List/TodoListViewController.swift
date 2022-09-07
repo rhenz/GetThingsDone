45da81c52ab7ccc93949069d841a1a7b94d02150
rhenz <salvador.rhenz@gmail.com>
2022-09-07 19:30:37 +0800

    Create `TodoListViewController` that will display all of the Todo Items

---
+++ GetThingsDone/Modules/Todo List/TodoListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TodoListViewController.swift
+//  GetThingsDone
+//
+//  Created by John Salvador on 9/7/22.
+//
+
+import UIKit
+
+class TodoListViewController: UIViewController {
+
+    // MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
