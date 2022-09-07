6449ab2275044fcbdbdc33c74f5073b3b61edf46
rhenz <salvador.rhenz@gmail.com>
2022-09-07 20:22:01 +0800

    Setup WelcomeViewController background view and its constraints

--- GetThingsDone/Modules/Welcome/WelcomeViewController.swift
+++ GetThingsDone/Modules/Welcome/WelcomeViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
+    // MARK: - Views
+    
+    private var backgroundView = UIView()
+    
     // MARK: - View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupViews()
+    }
+}
 
-        // Do any additional setup after loading the view.
+// MARK: - Setup UI
+
+extension WelcomeViewController {
+    private func setupViews() {
+        // View Controller's View
+        view.backgroundColor = .systemBackground
+        
+        // Background View
+        backgroundView.translatesAutoresizingMaskIntoConstraints = false
+        backgroundView.backgroundColor = .cyan
+        backgroundView.layer.cornerRadius = 6
+        view.addSubview(backgroundView)
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        let padding: CGFloat = 40
+        
+        // Background View
+        NSLayoutConstraint.activate([
+            backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            backgroundView.topAnchor.constraint(equalTo: view.topAnchor, constant: padding),
+            backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -padding),
+        ])
     }
 }
