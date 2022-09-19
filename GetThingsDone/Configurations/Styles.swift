4ae1b24bcd5505e234aecd51031980349253bf4c
rhenz <salvador.rhenz@gmail.com>
2022-09-19 20:51:31 +0800

    Update `Styles` constants to add font sizes

--- GetThingsDone/Configurations/Styles.swift
+++ GetThingsDone/Configurations/Styles.swift
@@ -9,9 +9,16 @@ import UIKit
 
 enum Styles {
     
+    // MARK: - Font
+    
     enum FontName {
         static let ralewayRegular = "Raleway-Regular"
         static let ralewayMedium = "Raleway-Medium"
         static let ralewayBold = "Raleway-Bold"
+    }
+    
+    enum FontSize {
+        static let regular: CGFloat = 16
+        static let largeTitle: CGFloat = 24
     }
 }
