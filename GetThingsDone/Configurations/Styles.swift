a477cbc66354a7b188b8f3601d09f6662f49cc40
rhenz <salvador.rhenz@gmail.com>
2022-09-19 20:46:15 +0800

    Add `Styles` constants that will hold mostly everything related to App Styling

---
+++ GetThingsDone/Configurations/Styles.swift
@@ -0,0 +1,17 @@
+//
+//  Styles.swift
+//  GetThingsDone
+//
+//  Created by John Salvador on 9/19/22.
+//
+
+import UIKit
+
+enum Styles {
+    
+    enum FontName {
+        static let ralewayRegular = "Raleway-Regular"
+        static let ralewayMedium = "Raleway-Medium"
+        static let ralewayBold = "Raleway-Bold"
+    }
+}
