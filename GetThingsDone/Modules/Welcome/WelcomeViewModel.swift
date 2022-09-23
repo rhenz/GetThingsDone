be5ea7d6b681a57c001adacac1938e0bba91ad7f
rhenz <salvador.rhenz@gmail.com>
2022-09-23 21:47:58 +0800

    Create `WelcomeViewModel` for `WelcomeViewController`

---
+++ GetThingsDone/Modules/Welcome/WelcomeViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  WelcomeViewModel.swift
+//  GetThingsDone
+//
+//  Created by John Salvador on 9/23/22.
+//
+
+import Foundation
+
+final class WelcomeViewModel {
+    
+    // MARK: - Properties
+    private let coordinator: WelcomeCoordinator
+    
+    // MARK: - Init
+    init(coordinator: WelcomeCoordinator) {
+        self.coordinator = coordinator
+    }
+    
+    // MARK: - Methods
+    func tappedNextButton() {
+        
+    }
+}
