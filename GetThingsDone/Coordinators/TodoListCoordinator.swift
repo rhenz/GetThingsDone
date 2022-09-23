3347890dc2c38676d7f638bb60e69bd39fa28f33
rhenz <salvador.rhenz@gmail.com>
2022-09-23 21:50:43 +0800

    Create `TodoListCoordinator` for `TodoListViewController`

---
+++ GetThingsDone/Coordinators/TodoListCoordinator.swift
@@ -0,0 +1,19 @@
+//
+//  TodoListCoordinator.swift
+//  GetThingsDone
+//
+//  Created by John Salvador on 9/23/22.
+//
+
+import Foundation
+
+final class TodoListCoordinator: Coordinator {
+    
+    // MARK: -
+    private(set) var childCoordinators: [Coordinator] = []
+    
+    // MARK: -
+    func start() {
+        
+    }
+}
