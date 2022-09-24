7958a89f8e2faf688044b96ed816b67f8a81db8a
rhenz <salvador.rhenz@gmail.com>
2022-09-24 23:23:11 +0800

    Setup header view and its constraints

--- GetThingsDone/Views/GTDHeaderView.swift
+++ GetThingsDone/Views/GTDHeaderView.swift
@@ -9,6 +9,9 @@ import UIKit
 
 final class GTDHeaderView: UIView {
     
+    // MARK: - Properties
+    let gradientView = GTDGradientView()
+    
     // MARK: - Init
     init(frame: CGRect = .zero, title: String = "header title", subtitle: String = "header subtitle") {
         super.init(frame: frame)
@@ -16,5 +19,23 @@ final class GTDHeaderView: UIView {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Setup UI
+extension GTDHeaderView {
+    private func setupView() {
+        translatesAutoresizingMaskIntoConstraints = false
+        addSubview(gradientView)
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            gradientView.topAnchor.constraint(equalTo: topAnchor),
+            gradientView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            gradientView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            gradientView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
     }
 }
