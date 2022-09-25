d2875addc20d1bf377826f1ba34330acfa54c8bf
rhenz <salvador.rhenz@gmail.com>
2022-09-25 22:00:45 +0800

    Update `GTDButton` initializer to add corner style customization

--- GetThingsDone/Views/Buttons/GTDButton.swift
+++ GetThingsDone/Views/Buttons/GTDButton.swift
@@ -12,10 +12,15 @@ class GTDButton: UIButton {
     // MARK: - Properties
     
     private(set) var title: String = "Button"
+    private let cornerStyle: UIButton.Configuration.CornerStyle
     
     // MARK: - Init
     
-    init(title: String, frame: CGRect = .zero) {
+    init(title: String,
+         frame: CGRect = .zero,
+         cornerStyle: UIButton.Configuration.CornerStyle = .capsule)
+    {
+        self.cornerStyle = cornerStyle
         super.init(frame: frame)
         self.title = title
         setupView()
@@ -37,7 +42,8 @@ extension GTDButton {
                                 size: Styles.FontSize.regular)
         
         var configuration = UIButton.Configuration.filled()
-        configuration.cornerStyle = .capsule
+        
+        configuration.cornerStyle = cornerStyle
         configuration.baseBackgroundColor = .white
         configuration.baseForegroundColor = .init(white: 0.6, alpha: 0.5)
         configuration.attributedTitle = AttributedString(title, attributes: container)
