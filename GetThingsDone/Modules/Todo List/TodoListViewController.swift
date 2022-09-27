4b23ffbc33de340e42cc4b072d2dfa96d03d8921
rhenz <salvador.rhenz@gmail.com>
2022-09-27 23:38:32 +0800

    Add notification center to handle keyboard show/hide for `PopUpView`

--- GetThingsDone/Modules/Todo List/TodoListViewController.swift
+++ GetThingsDone/Modules/Todo List/TodoListViewController.swift
@@ -13,12 +13,25 @@ class TodoListViewController: UIViewController {
     
     private let header = GTDHeaderView(headerTitle: "Stuff to get done")
     private let todoPopUpView = GTDTodoPopUpView()
+    
+    // MARK: - Properties
+    private var isTodoPopUpViewVisible = true
+    private var todoPopUpViewBottomConstraint = NSLayoutConstraint()
 
     // MARK: - View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
+        setupKeyboardObservers()
+    }
+}
+
+// MARK: - Helper Methods
+extension TodoListViewController {
+    private func setupKeyboardObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(sender:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(sender:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 }
 
@@ -49,11 +62,39 @@ extension TodoListViewController {
         ])
         
         // Todo Popup
+        todoPopUpViewBottomConstraint = todoPopUpView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         NSLayoutConstraint.activate([
-            todoPopUpView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            todoPopUpViewBottomConstraint,
             todoPopUpView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             todoPopUpView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
+    }
+}
+
+// MARK: - Actions
+extension TodoListViewController {
+    @objc func keyboardWillShow(sender: NSNotification) {
+        if let keyboardFrame: NSValue = sender.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue,
+           let keyboardDuration = sender.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? NSNumber
+        {
+            let keyboardHeight = keyboardFrame.cgRectValue.height
+            animatePopUpViewIfNeeded(withDuration: keyboardDuration.doubleValue, keyboardHeight: -keyboardHeight)
+        }
+    }
+    
+    @objc func keyboardWillHide(sender: NSNotification) {
+        if let keyboardDuration = sender.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? NSNumber {
+            animatePopUpViewIfNeeded(withDuration: keyboardDuration.doubleValue, keyboardHeight: 0)
+        }
+    }
+    
+    func animatePopUpViewIfNeeded(withDuration duration: TimeInterval, keyboardHeight: CGFloat) {
+        if isTodoPopUpViewVisible {
+            todoPopUpViewBottomConstraint.constant = keyboardHeight
+            UIView.animate(withDuration: duration) {
+                self.view.layoutIfNeeded()
+            }
+        }
     }
 }
 
