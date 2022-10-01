42aa82fc6f397c29f98b2ff1ea17fd8ff2b0d45e
rhenz <salvador.rhenz@gmail.com>
2022-10-01 23:20:34 +0800

    Create initial custom table view file

---
+++ GetThingsDone/Views/TableViews/GTDTableView.swift
@@ -0,0 +1,20 @@
+//
+//  GTDTableView.swift
+//  GetThingsDone
+//
+//  Created by John Salvador on 10/1/22.
+//
+
+import UIKit
+
+class GTDTableView: UITableView {
+    
+    // MARK: - Init
+    override init(frame: CGRect, style: UITableView.Style) {
+        super.init(frame: frame, style: style)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
