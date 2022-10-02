79aeb6c7fa7824364f1e7ddde6145207421258b7
rhenz <salvador.rhenz@gmail.com>
2022-10-02 11:16:19 +0800

    Update custom table view design

--- GetThingsDone/Views/TableViews/GTDTableView.swift
+++ GetThingsDone/Views/TableViews/GTDTableView.swift
@@ -12,9 +12,19 @@ class GTDTableView: UITableView {
     // MARK: - Init
     override init(frame: CGRect, style: UITableView.Style) {
         super.init(frame: frame, style: style)
+        setupView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Setup UI
+extension GTDTableView {
+    private func setupView() {
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .clear
+        separatorStyle = .none
     }
 }
