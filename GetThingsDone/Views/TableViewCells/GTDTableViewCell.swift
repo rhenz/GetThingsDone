e8a822d0d7904358674c2cfffeca6c616aa47811
rhenz <salvador.rhenz@gmail.com>
2022-10-02 15:45:02 +0800

    Update table view cell design to add a label

--- GetThingsDone/Views/TableViewCells/GTDTableViewCell.swift
+++ GetThingsDone/Views/TableViewCells/GTDTableViewCell.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class GTDTableViewCell: UITableViewCell {
     
+    // MARK: - Views
+    var titleLabel = GTDLabel()
+    private var todoItemView = UIView()
+    
     // MARK: - Init
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -19,27 +23,43 @@ class GTDTableViewCell: UITableViewCell {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    // MARK: - Views
-    var titleLabel = GTDLabel()
 }
 
 // MARK: - Setup UI
 extension GTDTableViewCell {
     private func style() {
-        contentView.layer.cornerRadius = 6
+        backgroundColor = .clear
+        selectionStyle = .none
         
-        titleLabel.text = "Sample Todo"
-        contentView.addSubview(titleLabel)
+        todoItemView.translatesAutoresizingMaskIntoConstraints = false
+        todoItemView.layer.cornerRadius = 6
+        todoItemView.backgroundColor = .white
+        contentView.addSubview(todoItemView)
+        
+        titleLabel.textColor = Styles.Color.todoItemLabel
+        todoItemView.addSubview(titleLabel)
     }
     
     private func layout() {
-        // Title Label
+        // TodoItem View
+        let topAndBottomPadding: CGFloat = 5
+        let leftAndRightPadding: CGFloat = 15
+        let todoItemViewHeight: CGFloat = 29
         NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor),
-            titleLabel.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
-            titleLabel.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor),
-            titleLabel.bottomAnchor.constraint(equalTo: contentView.layoutMarginsGuide.bottomAnchor)
+            todoItemView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: topAndBottomPadding),
+            todoItemView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: leftAndRightPadding),
+            todoItemView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -leftAndRightPadding),
+            todoItemView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -topAndBottomPadding),
+            todoItemView.heightAnchor.constraint(equalToConstant: todoItemViewHeight)
+        ])
+        
+        // Title Label
+        let labelPadding: CGFloat = 5
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: todoItemView.topAnchor, constant: labelPadding),
+            titleLabel.leadingAnchor.constraint(equalTo: todoItemView.leadingAnchor, constant: labelPadding),
+            titleLabel.trailingAnchor.constraint(equalTo: todoItemView.trailingAnchor, constant: -labelPadding),
+            titleLabel.bottomAnchor.constraint(equalTo: todoItemView.bottomAnchor, constant: -labelPadding),
         ])
     }
     
